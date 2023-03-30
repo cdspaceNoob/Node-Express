a8adbc3d27d90af2d90271b12a3d9ab42f8acc04
voyager <cdspacenoob@gmail.com>
2023-03-30 16:21:09 +0900

    how express works with routing

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-// use: 새로운 미들웨어를 등록.
-// use()의 callback은 모든 요청에 대해 실행.
-// next: Express로부터 받은 함수로, 다음 미들웨어로 넘어갈 수 있게 한다.
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In the middleware!');
     next();
-});
-
-app.use((req, res, next) => {
-    console.log('In another middleware!');
 })
 
-const server = http.createServer(app);
+app.use('/add', (req, res, next) => {
+    console.log('In /add path!');
+    res.send("<h1>Hello from path /add!</h1>")
+})
 
-server.listen(3000); 
+app.use('/', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send("<h1>Hello from Express!</h1>")
+})
+
+app.listen(3000);
+// const server = http.createServer(app);
+// server.listen(3000); 
