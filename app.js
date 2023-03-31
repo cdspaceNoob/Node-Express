70eaa4c8727e75b0d464982881593dd4782c65fb
voyager <cdspacenoob@gmail.com>
2023-03-31 13:37:01 +0900

    router filter

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('In the middleware!');
-    next();
-})
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add', (req, res, next) => {
-    console.log('In /add path!');
-    res.send("<h1>Hello from path /add!</h1>")
-})
+// extended: false means that the values can be strings or arrays
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send("<h1>Hello from Express!</h1>")
-})
+  res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(3000);
 // const server = http.createServer(app);
-// server.listen(3000); 
+// server.listen(3000);
